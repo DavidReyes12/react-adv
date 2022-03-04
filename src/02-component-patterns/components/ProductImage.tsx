7600fa4f6727a826041f574022dc2ab215ad9c63
davidyo <david_changui@hotmail.com>
2022-03-04 17:32:18 -0600

    Component Extend Styles - ready

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -1,11 +1,16 @@
-import { useContext } from "react";
+import { useContext, CSSProperties } from 'react';
 import { ProductContext } from './ProductCard';
 
 import noImage from "../assets/no-image.jpg";
 import styles from "../styles/styles.module.css";
 
+export interface Props {
+    image?: string;
+    className?: string;
+    style?: CSSProperties;
+};
 
-export const ProductImage = ({ image = "" }) => {
+export const ProductImage = ({ image, className, style }: Props) => {
 
     const { product: { img } } = useContext(ProductContext);
     let imgToShow: string;
@@ -19,6 +24,11 @@ export const ProductImage = ({ image = "" }) => {
     };
 
     return (
-        <img className={styles.productImg} src={ imgToShow } alt="Product" />
+        <img 
+            alt="Product" 
+            className={`${ styles.productImg } ${ className }`} 
+            src={ imgToShow } 
+            style={ style }
+        />
     );
 };
--- src/02-component-patterns/components/ProductTitle.tsx
+++ src/02-component-patterns/components/ProductTitle.tsx
@@ -1,14 +1,25 @@
-import { useContext } from "react";
+import { useContext, CSSProperties } from 'react';
 import { ProductContext } from './ProductCard';
 
 import styles from "../styles/styles.module.css";
 
+export interface Props {
+    className?: string;
+    title?: string;
+    style?: CSSProperties;
+};
 
-export const ProductTitle = ({ title }: { title?: string }) => {
+
+export const ProductTitle = ({ title, className, style }: Props) => {
 
     const { product: { title: titleContext } } = useContext(ProductContext);
 
     return (
-        <span className={ styles.productDescription } > { title ? title : titleContext } </span>
+        <span 
+            className={ `${ styles.productDescription } ${ className }` } 
+            style={ style }
+        >
+             { title ? title : titleContext } 
+        </span>
     );
 };
