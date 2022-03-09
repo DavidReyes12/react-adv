8a93bc09d6a979fed21f2e47b59b5ec737953421
davidyo <david_changui@hotmail.com>
2022-03-09 17:47:56 -0600

    pattern completed

--- src/02-component-patterns/components/ProductButtons.tsx
+++ src/02-component-patterns/components/ProductButtons.tsx
@@ -1,4 +1,4 @@
-import { useContext, CSSProperties } from 'react';
+import { useContext, CSSProperties, useCallback } from 'react';
 import { ProductContext } from './ProductCard';
 
 import styles from "../styles/styles.module.css";
@@ -11,7 +11,12 @@ export interface Props {
 
 export const ProductButtons = ({ className, style }: Props) => {
 
-    const { increaseBy, counter } = useContext(ProductContext);
+    const { increaseBy, counter, maxCount } = useContext(ProductContext);
+
+    const isMaxReached = useCallback(
+        () => !!maxCount && counter === maxCount,
+        [ counter, maxCount ],
+    );
 
     return (
         <div 
@@ -26,7 +31,7 @@ export const ProductButtons = ({ className, style }: Props) => {
             </button>
             <div className={ styles.countLabel } > { counter } </div>
             <button 
-                className={ styles.buttonAdd }
+                className={ `${ styles.buttonAdd } ${ isMaxReached() && styles.disabled }` }
                 onClick={ () => increaseBy( 1 ) }
             >
                 +
